f11b76a5eef48d98a7b3b72b21769c3eb450ec76
TOCOPREGUICA <felipeborgesmelo80@gmail.com>
2026-05-14 15:03:36 -0300

    video

--- backend/livro/src/test/java/com/hyperminetec/livro/controller/AutenticacaoApiTest.java
+++ backend/livro/src/test/java/com/hyperminetec/livro/controller/AutenticacaoApiTest.java
@@ -11,10 +11,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import static io.restassured.RestAssured.given;
+import io.restassured.response.Response;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class AutenticacaoApiTest {
 
@@ -23,7 +24,7 @@ public class AutenticacaoApiTest {
 
     @BeforeEach
     void setUp() {
-        RestAssured.port = port;
+        RestAssured.port = 8080;
     }
 
     @Test
@@ -31,31 +32,47 @@ public class AutenticacaoApiTest {
         UsuarioRequestDTO novoUsuario = new UsuarioRequestDTO("Maria", "maria@email.com", "senha123", Role.CLIENTE);
 
         given()
-            .contentType(ContentType.JSON)
-            .body(novoUsuario)
-        .when()
-            .post("/api/usuarios")
-        .then()
-            .statusCode(201)
-            .body("nome", equalTo("Maria"))
-            .body("email", equalTo("maria@email.com"))
-            .body("role", equalTo("CLIENTE"))
-            .body("id", notNullValue()); 
+                .contentType(ContentType.JSON)
+                .body(novoUsuario)
+                .when()
+                .post("/api/usuarios")
+                .then()
+                .statusCode(201)
+                .body("nome", equalTo("Maria"))
+                .body("email", equalTo("maria@email.com"))
+                .body("role", equalTo("CLIENTE"))
+                .body("id", notNullValue());
     }
 
     @Test
     void deveEfetuarLoginERetornarTokenRoleEIdStatus200() {
-         LoginRequestDTO login = new LoginRequestDTO("joao@vendas.com", "cliente123");
 
-        given()
-            .contentType(ContentType.JSON)
-            .body(login)
-        .when()
-            .post("/api/login")
-        .then()
-            .statusCode(200) 
-            .body("token", notNullValue())
-            .body("role", equalTo("CLIENTE"))
-            .body("id", notNullValue()); 
+        LoginRequestDTO login
+                = new LoginRequestDTO("joao@vendas.com", "cliente123");
+
+        Response resposta = given()
+                .contentType(ContentType.JSON)
+                .body(login)
+                .log().all()
+                .when()
+                .post("/api/login")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("token", notNullValue())
+                .body("role", equalTo("CLIENTE"))
+                .body("id", notNullValue())
+                .extract()
+                .response();
+
+        String token = resposta.jsonPath().getString("token");
+        String role = resposta.jsonPath().getString("role");
+        Integer id = resposta.jsonPath().getInt("id");
+
+        System.out.println("\n======= DADOS DO LOGIN =======");
+        System.out.println("TOKEN: " + token);
+        System.out.println("ROLE: " + role);
+        System.out.println("ID: " + id);
+        System.out.println("==============================");
     }
 }
